bc56be70da7beb64798c703d561753f1d359c616
Adrien Brault <adrien.brault@gmail.com>
2014-11-19 21:08:23 +0100

    Fix remove_offline_nodes recipe

---
+++ files/default/remove_offline_nodes.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+cd /tmp
+
+rm -f {nodes,running_nodes,offline_nodes}.txt
+
+rabbitmqctl cluster_status | grep running_nodes | grep -oE "\w+@\w+" | sort > running_nodes.txt
+rabbitmqctl cluster_status | grep "\[{nodes," | grep -oE "\w+@\w+" | sort > nodes.txt
+
+comm -23 nodes.txt running_nodes.txt > offline_nodes.txt
+
+cat offline_nodes.txt | while read node
+do
+  rabbitmqctl forget_cluster_node $node
+done
--- recipes/remove_offline_nodes.rb
+++ recipes/remove_offline_nodes.rb
@@ -1,16 +1,8 @@
-bash 'remove_offline_nodes' do
-  cwd '/tmp'
-  code <<-EOH
-    rm -f {nodes,running_nodes,offline_nodes}.txt
-
-    rabbitmqctl cluster_status | grep running_nodes | grep -oE "\w+@\w+" | sort > running_nodes.txt
-    rabbitmqctl cluster_status | grep "\[{nodes," | grep -oE "\w+@\w+" | sort > nodes.txt
-
-    comm -23 nodes.txt running_nodes.txt > offline_nodes.txt
-
-    cat offline_nodes.txt | while read node
-    do
-      rabbitmqctl forget_cluster_node $node
-    done
-  EOH
+cookbook_file "/usr/local/bin/remove_offline_nodes" do
+  source 'remove_offline_nodes.sh'
+  owner 'root'
+  group 'root'
+  mode '770'
 end
+
+execute '/usr/local/bin/remove_offline_nodes'
